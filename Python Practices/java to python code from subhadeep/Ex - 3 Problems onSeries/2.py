f6ab1f5c2a7ecb51da9ffd916c235a3504cf6e55
mukherjeeritwik3 <65210468+mukherjeeritwik3@users.noreply.github.com>
2021-04-08 16:55:00 +0530

    d

--- Python Practices/java to python code from subhadeep/Ex - 3 Problems onSeries/2.py
+++ Python Practices/java to python code from subhadeep/Ex - 3 Problems onSeries/2.py
@@ -1,9 +1,31 @@
-n = int(input())
-index = 0
-temp = 0
-for i in range(0,n):
-    if i == 0:
-        print(temp)
-        index+=1
+def binary_search(arr, low, high, x):
+    # Check base case
+    count = 0
+    if high >= low:
 
-    temp =
+        mid = (high + low) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+
+    else:
+        # Element is not present in the array
+        return -1
+
+
+# Test array
+arr = [2, 3, 4, 10, 40]
+x = 10
+
+# Function call
+result = binary_search(arr, 0, len(arr) - 1, x)
